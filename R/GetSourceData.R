6cb8e385026f0edf0105a1f5ffe6beeca804d11c
Alex Antonison <alex@antonison-cg.com>
2023-02-15 10:50:16 -0600

    Format code

--- R/GetSourceData.R
+++ R/GetSourceData.R
@@ -1,15 +1,13 @@
-
 get_source_data <- function() {
-  
   s3 <- paws::s3()
-  
+
   bucket_name <- "univ-of-fl-data-collaboration"
-  
+
   s3_obj_list <- s3$list_objects_v2(
     Bucket = bucket_name,
     Prefix = "data/source/"
   )
-  
+
   for (obj in s3_obj_list$Contents) {
     object_key <- obj$Key
     local_file_path <- paste0(
