2ae6e88986410838e2e71b4fbb5a55fa093030cb
Alex Antonison <alex@antonison-cg.com>
2023-03-01 14:29:56 -0600

    Streamline putting source files into s3

--- R/PutSourceData.R
+++ R/PutSourceData.R
@@ -12,3 +12,20 @@ upload_file_to_s3 <- function(file_path, file_name) {
     Bucket = "univ-of-fl-data-collaboration"
   )
 }
+
+# do not upload non-data files to s3
+exclude_files <- c(".DS_Store", ".gitignore", "README.md")
+
+for (file_path in fs::dir_ls("data/source/")) {
+  
+  # extract file_name from path
+  file_name <- fs::path_file(file_path)
+  
+  if (file_name %in% exclude_files) {
+    print(paste0("Exclude: ", file_name))
+  } else {
+    print(paste0("Uploading: ", file_name, " to S3"))
+    upload_file_to_s3("data/source/", file_name)
+  }
+  
+}
