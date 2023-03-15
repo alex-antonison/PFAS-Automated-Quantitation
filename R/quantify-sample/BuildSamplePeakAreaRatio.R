1023c2dbfbaad9749d795e1f56b8cd374a43401e
Alex Antonison <alex@antonison-cg.com>
2023-03-15 13:35:10 -0500

    Formatting

--- R/quantify-sample/BuildSamplePeakAreaRatio.R
+++ R/quantify-sample/BuildSamplePeakAreaRatio.R
@@ -1,7 +1,7 @@
 #' Building the Peak Area Ratio Table with Samples
-#' 
+#'
 #' This will use the the samples from the source data
-#' 
+#'
 #' Input:
 #'   Data:
 #'     data/processed/source/sample_individual_native_analyte.parquet
@@ -20,7 +20,7 @@ library(magrittr)
 sample_native_analyte_df <- arrow::read_parquet(
   "data/processed/source/sample_individual_native_analyte.parquet"
 )
-  
+
 
 sample_internal_standard_df <- arrow::read_parquet(
   "data/processed/source/sample_internal_standard.parquet"
@@ -28,24 +28,24 @@ sample_internal_standard_df <- arrow::read_parquet(
 
 native_analyte_internal_standard_mapping_df <- arrow::read_parquet(
   "data/processed/reference/native_analyte_internal_standard_mapping.parquet"
-  )
+)
 
-sample_native_analyte_df %>% 
+sample_native_analyte_df %>%
   dplyr::left_join(
     native_analyte_internal_standard_mapping_df,
     by = "individual_native_analyte_name"
-  ) %>% 
+  ) %>%
   dplyr::left_join(
     sample_internal_standard_df,
     by = c("internal_standard_name", "cartridge_number", "batch_number")
-  ) %>% 
+  ) %>%
   dplyr::mutate(
     # calculate the peak area ratio
     peak_area_ratio = individual_native_analyte_peak_area / internal_standard_peak_area
-  ) %>% 
+  ) %>%
   arrow::write_parquet(
     sink = "data/processed/quantify-sample/peak_area_ratio.parquet"
-  ) %>% 
+  ) %>%
   readr::write_excel_csv(
     "data/processed/quantify-sample/peak_area_ratio.csv"
   )
