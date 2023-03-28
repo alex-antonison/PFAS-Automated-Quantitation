43132f580270eb93797457c6daa973baddd4b6f5
Alex Antonison <alex@antonison-cg.com>
2023-03-28 08:46:46 -0500

    Add method for using the default name if the source name matches

--- R/build-calibration-curve/1_CalculateConcentrationRatio.R
+++ R/build-calibration-curve/1_CalculateConcentrationRatio.R
@@ -1,31 +1,82 @@
 #' Calculate the Concentration Ratio
 #'
-#'
+#' Input:
+#'   - Sep2021Calibration_Curve_source.xlsx
+#'   - Reference mapping between Native Analyte and Internal Standard
+#'   - Reference mapping between Native Analyte names in source data and calibration curve
+
 
 library(magrittr)
 
 source("R/process-source-data/ProcessCalibrationCurveSourceFile.R")
 source("R/process-source-data/RefCreateMappingFiles.R")
 
-native_analyte_concentration_df <- arrow::read_parquet("data/processed/native_analyte_concentration.parquet")
+# processed from Sep2021Calibration_Curve_source.xlsx
+analyte_concentration_df <- arrow::read_parquet(
+  "data/processed/native_analyte_concentration.parquet"
+  )
 
-internal_standard_concentration_df <- arrow::read_parquet("data/processed/internal_standard_concentration.parquet")
+# processed from Sep2021Calibration_Curve_source.xlsx
+internal_standard_concen_df <- arrow::read_parquet(
+  "data/processed/internal_standard_concentration.parquet"
+  )
 
-native_analyte_internal_standard_mapping_df <- arrow::read_parquet("data/processed/reference/native_analyte_internal_standard_mapping.parquet")
+# native analyte to internal standard mapping
+native_analyte_internal_standard_mapping_df <- arrow::read_parquet(
+  "data/processed/reference/native_analyte_internal_standard_mapping.parquet"
+  )
 
-cal_name_native_analyte_mapping_df <- arrow::read_parquet("data/processed/reference/calibration_concentration_name_mapping.parquet")
+# native analyte name mapping between source
+# and Sep2021Calibration_Curve_source.xlsx
+cal_name_native_analyte_mapping_df <- arrow::read_parquet(
+  "data/processed/reference/calibration_concentration_name_mapping.parquet"
+  ) %>% 
+  dplyr::rename(mapped_analyte_name = individual_native_analyte_name)
 
-native_analyte_concentration_df %>%
+concen_internal_stanard_mapping <- readxl::read_excel(
+  "data/source/concentration_internal_standard_mapping.xlsx"
+  )
+
+# TODO - when the names line up in SepCalibration file, use that. Only use the mapping file for names that
+# do not have a matching analyte.
+
+temp_df <- analyte_concentration_df %>%
   dplyr::rename(source_analyte_name = individual_native_analyte_name) %>%
-  dplyr::left_join(cal_name_native_analyte_mapping_df, by = "source_analyte_name") %>%
+  # dplyr::filter(stringr::str_detect(source_analyte_name, "6:2FTS")) %>% 
+  dplyr::left_join(
+    cal_name_native_analyte_mapping_df, by = "source_analyte_name"
+    ) %>%
+  dplyr::mutate(
+    individual_native_analyte_name = ifelse(is.na(mapped_analyte_name),
+                                            source_analyte_name,
+                                            mapped_analyte_name)
+  ) %>%
   # dropping instances where there is not a mapped analyte name TODO
   dplyr::filter(!is.na(individual_native_analyte_name)) %>%
-  dplyr::left_join(native_analyte_internal_standard_mapping_df, by = c("individual_native_analyte_name")) %>%
-  dplyr::left_join(internal_standard_concentration_df, by = c("internal_standard_name", "calibration_mix", "calibration_level")) %>%
+  dplyr::left_join(
+    native_analyte_internal_standard_mapping_df,
+    by = c("individual_native_analyte_name")
+    ) %>%
+  dplyr::rename(
+    source_internal_standard_name = internal_standard_name
+  ) %>% 
+  dplyr::left_join(
+    concen_internal_stanard_mapping,
+   by = c("source_internal_standard_name" = "mapped_internal_standard_name")
+   ) %>% 
+  dplyr::mutate(
+    internal_standard_name = ifelse(is.na(concentration_internal_standard_name),
+                                    source_internal_standard_name,
+                                    concentration_internal_standard_name)
+  ) %>% 
+  dplyr::left_join(
+    internal_standard_concen_df,
+    by = c("internal_standard_name", "calibration_mix", "calibration_level")
+    ) %>% 
   # remove instances where there are not mapped calibration values TODO
   dplyr::filter(!is.na(internal_standard_concentration_ppt)) %>%
   dplyr::mutate(
-    analyte_concentration_ratio = native_analyte_concentration_ppt / internal_standard_concentration_ppt
+    analyte_concentration_ratio = native_analyte_concentration_ppt / internal_standard_concentration_ppt # nolint
   ) %>%
   dplyr::select(
     individual_native_analyte_name,
@@ -39,4 +90,7 @@ native_analyte_concentration_df %>%
   arrow::write_parquet(
     sink = "data/processed/calibration-curve/concentration_ratio.parquet"
   ) %>%
-  readr::write_excel_csv("data/processed/calibration-curve/concentration_ratio.csv")
+  readr::write_excel_csv(
+    "data/processed/calibration-curve/concentration_ratio.csv"
+    )
+  # head(50)
