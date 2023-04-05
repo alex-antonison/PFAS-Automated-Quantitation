970f87f361ba4a1c132905e8f8d273e1b05a2bde
Alex Antonison <alex@antonison-cg.com>
2023-04-05 15:51:32 -0500

    Formatting

--- R/build-calibration-curve/2_BuildCalibrationCurveInput.R
+++ R/build-calibration-curve/2_BuildCalibrationCurveInput.R
@@ -18,7 +18,7 @@ source("R/build-calibration-curve/1_CalculateConcentrationRatio.R")
 
 average_peak_area_ratio_df <- arrow::read_parquet(
   "data/processed/calibration-curve/average_peak_area_ratio.parquet"
-  )
+)
 
 concentration_ratio_df <- arrow::read_parquet("data/processed/calibration-curve/concentration_ratio.parquet") %>%
   dplyr::select(
@@ -29,15 +29,15 @@ concentration_ratio_df <- arrow::read_parquet("data/processed/calibration-curve/
 
 analyte_to_internal_standard_mapping <- arrow::read_parquet(
   "data/processed/reference/native_analyte_internal_standard_mapping.parquet"
-) %>% 
+) %>%
   # TODO: Remove when reference file updated to include actual value
   dplyr::mutate(
     minimum_limit_of_quantitation = "Cal 2"
-  ) %>% 
+  ) %>%
   # pull out number
   dplyr::mutate(
     minimum_limit_of_quantitation = readr::parse_number(minimum_limit_of_quantitation)
-  ) %>% 
+  ) %>%
   dplyr::select(
     individual_native_analyte_name,
     minimum_limit_of_quantitation
@@ -54,16 +54,16 @@ average_peak_area_ratio_df %>%
   dplyr::left_join(
     analyte_to_internal_standard_mapping,
     by = c("individual_native_analyte_name")
-  ) %>% 
+  ) %>%
   # remove calibration levels less than the limit of quantation
   dplyr::filter(
     calibration_level >= minimum_limit_of_quantitation
-  ) %>% 
+  ) %>%
   # remove minimum limit of quantation from dataframe prior
   # to creating calibration curve input
   dplyr::select(
     -minimum_limit_of_quantitation
-  ) %>% 
+  ) %>%
   arrow::write_parquet(
     sink = "data/processed/calibration-curve/calibration_curve_input.parquet"
   ) %>%
