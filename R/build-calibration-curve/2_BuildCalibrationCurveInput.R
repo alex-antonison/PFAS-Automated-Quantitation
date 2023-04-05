7501e76c3fa236cf5027ec266969c528c247b211
Alex Antonison <alex@antonison-cg.com>
2023-04-05 14:17:07 -0500

    Add temp logic for calculating calibration curve input

--- R/build-calibration-curve/2_BuildCalibrationCurveInput.R
+++ R/build-calibration-curve/2_BuildCalibrationCurveInput.R
@@ -16,13 +16,31 @@ library(magrittr)
 source("R/build-calibration-curve/1_CalculateAveragePeakRatio.R")
 source("R/build-calibration-curve/1_CalculateConcentrationRatio.R")
 
-average_peak_area_ratio_df <- arrow::read_parquet("data/processed/calibration-curve/average_peak_area_ratio.parquet")
+average_peak_area_ratio_df <- arrow::read_parquet(
+  "data/processed/calibration-curve/average_peak_area_ratio.parquet"
+  )
 
 concentration_ratio_df <- arrow::read_parquet("data/processed/calibration-curve/concentration_ratio.parquet") %>%
   dplyr::select(
     individual_native_analyte_name,
     calibration_level,
     analyte_concentration_ratio
+  )
+
+analyte_to_internal_standard_mapping <- arrow::read_parquet(
+  "data/processed/reference/native_analyte_internal_standard_mapping.parquet"
+) %>% 
+  # TODO: Remove when reference file updated to include actual value
+  dplyr::mutate(
+    minimum_limit_of_quantitation = "Cal 2"
+  ) %>% 
+  # pull out number
+  dplyr::mutate(
+    minimum_limit_of_quantitation = readr::parse_number(minimum_limit_of_quantitation)
+  ) %>% 
+  dplyr::select(
+    individual_native_analyte_name,
+    minimum_limit_of_quantitation
   )
 
 average_peak_area_ratio_df %>%
@@ -33,11 +51,22 @@ average_peak_area_ratio_df %>%
       "calibration_level"
     )
   ) %>%
+  dplyr::left_join(
+    analyte_to_internal_standard_mapping,
+    by = c("individual_native_analyte_name")
+  ) %>% 
+  # remove calibration levels less than the limit of quantation
+  dplyr::filter(
+    calibration_level >= minimum_limit_of_quantitation
+  ) %>% 
+  # remove minimum limit of quantation from dataframe prior
+  # to creating calibration curve input
+  dplyr::select(
+    -minimum_limit_of_quantitation
+  ) %>% 
   arrow::write_parquet(
     sink = "data/processed/calibration-curve/calibration_curve_input.parquet"
   ) %>%
-  as.data.frame() %>%
-  xlsx::write.xlsx(
-    "data/processed/calibration-curve/calibration_curve_input.xlsx",
-    row.names = FALSE
+  readr::write_csv(
+    "data/processed/calibration-curve/calibration_curve_input.csv"
   )
