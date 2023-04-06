407498932263efb23b7c8c6c70871966fa326e3f
Alex Antonison <alex@antonison-cg.com>
2023-04-06 14:55:41 -0500

    Refactor to better organize files

--- R/utility/GetSourceData.R
+++ R/utility/GetSourceData.R
@@ -5,20 +5,18 @@ get_source_data <- function() {
 
   s3_obj_list <- s3$list_objects_v2(
     Bucket = bucket_name,
-    Prefix = "data/source/"
+    Prefix = "data/source/",
+    
   )
 
   for (obj in s3_obj_list$Contents) {
     object_key <- obj$Key
-    local_file_path <- paste0(
-      "data/source/",
-      fs::path_split(object_key)[[1]][3]
-    )
-    print(local_file_path)
+    print(object_key)
+    
     s3$download_file(
       Bucket = bucket_name,
       Key = object_key,
-      Filename = local_file_path
+      Filename = object_key
     )
   }
 }
