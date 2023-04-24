069c3b0de13a16624f2cc89486a43c0e9bbe5ab3
Alex Antonison <alex@antonison-cg.com>
2023-04-24 09:32:20 -0500

    Formatting and notes

--- R/build-data-products/1_BuildExtractionBlank.R
+++ R/build-data-products/1_BuildExtractionBlank.R
@@ -20,25 +20,25 @@ analyte_peak_area <- analyte_concentration_df %>%
   #   cartridge_number = filename,
   #   # convert peak area to numeric
   #   area_with_nan = dplyr::na_if(area, "NF"),
-#   individual_native_analyte_peak_area = as.numeric(area_with_nan),
-#   # calculate analyte name
-#   analyte_name_length = stringr::str_length(sheet_name),
-#   individual_native_analyte_name = stringr::str_sub(sheet_name, 0, analyte_name_length - 2),
-#   # calculate transition number
-#   transition_number = stringr::str_sub(sheet_name, -1)
-# ) %>%
-# dplyr::filter(transition_number == 1) %>%
-dplyr::select(
-  batch_number,
-  individual_native_analyte_name,
-  cartridge_number,
-  calibration_curve_range_category,
-  analyte_concentration_ng = analyte_concentration
-)
+  #   individual_native_analyte_peak_area = as.numeric(area_with_nan),
+  #   # calculate analyte name
+  #   analyte_name_length = stringr::str_length(sheet_name),
+  #   individual_native_analyte_name = stringr::str_sub(sheet_name, 0, analyte_name_length - 2),
+  #   # calculate transition number
+  #   transition_number = stringr::str_sub(sheet_name, -1)
+  # ) %>%
+  # dplyr::filter(transition_number == 1) %>%
+  dplyr::select(
+    batch_number,
+    individual_native_analyte_name,
+    cartridge_number,
+    calibration_curve_range_category,
+    analyte_concentration_ng = analyte_concentration
+  )
 
 
 cur_df <- analyte_peak_area %>%
-  dplyr::filter(batch_number == 2) %>% 
+  dplyr::filter(batch_number == 2) %>%
   dplyr::left_join(
     extraction_batch_source,
     by = c("batch_number", "cartridge_number")
@@ -61,11 +61,11 @@ cur_df <- analyte_peak_area %>%
     sample_id,
     sample_mass_g,
     analyte_concentration_ng
-  ) #%>% 
+  ) # %>%
 # dplyr::group_by(
 #   batch_number,
 #   individual_native_analyte_name
-#   ) %>% 
+#   ) %>%
 # dplyr::summarise(
 #   average_analyte_concentration_ng = mean(analyte_concentration_ng)
 # )
