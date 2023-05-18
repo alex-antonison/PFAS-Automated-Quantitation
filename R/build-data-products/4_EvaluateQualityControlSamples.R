eae5fc7bd44761d7c540101911030cee157cb568
Alex Antonison <adantonison@gmal.com>
2023-05-18 13:53:27 -0500

    Formatting

--- R/build-data-products/4_EvaluateQualityControlSamples.R
+++ R/build-data-products/4_EvaluateQualityControlSamples.R
@@ -6,28 +6,27 @@ source("R/process-source-data/ProcessQCSampleFile.R")
 eval_qc_for_blank_filtered_analyte <- function(blank_filtered_analyte_concentration_quality_control,
                                                native_analyte_quality_control_levels,
                                                file_name) {
-
-blank_filtered_analyte_concentration_quality_control %>% 
-  dplyr::left_join(
-    native_analyte_quality_control_levels,
-    by = c("individual_native_analyte_name", "qc_level")
-  ) %>% 
-  dplyr::mutate(
-    qc_recovery_ratio = (blank_filtered_average_qc_analyte_concentration_ng / native_analyte_spiked_in_qc_samples_ng) * 100
-  ) %>% 
-  dplyr::select(
-    batch_number,
-    individual_native_analyte_name,
-    qc_level,
-    blank_filtered_average_qc_analyte_concentration_ng,
-    native_analyte_spiked_in_qc_samples_ng,
-    qc_recovery_ratio
-  ) %>% 
-  readr::write_excel_csv(
-    paste0("data/processed/build-data-products/blank_filtered_evaluated_qc_",file_name,".csv")
-  ) %>%
+  blank_filtered_analyte_concentration_quality_control %>%
+    dplyr::left_join(
+      native_analyte_quality_control_levels,
+      by = c("individual_native_analyte_name", "qc_level")
+    ) %>%
+    dplyr::mutate(
+      qc_recovery_ratio = (blank_filtered_average_qc_analyte_concentration_ng / native_analyte_spiked_in_qc_samples_ng) * 100
+    ) %>%
+    dplyr::select(
+      batch_number,
+      individual_native_analyte_name,
+      qc_level,
+      blank_filtered_average_qc_analyte_concentration_ng,
+      native_analyte_spiked_in_qc_samples_ng,
+      qc_recovery_ratio
+    ) %>%
+    readr::write_excel_csv(
+      paste0("data/processed/build-data-products/blank_filtered_evaluated_qc_", file_name, ".csv")
+    ) %>%
     arrow::write_parquet(
-      sink = paste0("data/processed/build-data-products/blank_filtered_evaluated_qc_",file_name,".parquet")
+      sink = paste0("data/processed/build-data-products/blank_filtered_evaluated_qc_", file_name, ".parquet")
     )
 }
 
@@ -43,10 +42,14 @@ native_analyte_quality_control_levels <- arrow::read_parquet(
 )
 
 
-eval_qc_for_blank_filtered_analyte(blank_filtered_analyte_concentration_quality_control_no_recovery,
-                                   native_analyte_quality_control_levels,
-                                   "no_recovery")
+eval_qc_for_blank_filtered_analyte(
+  blank_filtered_analyte_concentration_quality_control_no_recovery,
+  native_analyte_quality_control_levels,
+  "no_recovery"
+)
 
-eval_qc_for_blank_filtered_analyte(blank_filtered_analyte_concentration_quality_control_with_recovery,
-                                   native_analyte_quality_control_levels,
-                                   "with_recovery")
+eval_qc_for_blank_filtered_analyte(
+  blank_filtered_analyte_concentration_quality_control_with_recovery,
+  native_analyte_quality_control_levels,
+  "with_recovery"
+)
