28aad994d8eab22f476a319bbcd26331e5d1cec5
Alexander Antonison <adantonison@gmail.com>
2023-08-26 13:05:05 -0500

    Add run all prep

---
+++ R/process-source-data/RunAllPrep.R
@@ -0,0 +1,7 @@
+source("R/process-source-data/RefCreateMappingFiles.R")
+source("R/process-source-data/ProcessRawData.R")
+source("R/process-source-data/ProcessQCSampleFile.R")
+source("R/process-source-data/ProcessInternalStandardMixFile.R")
+source("R/process-source-data/ProcessExtractionBatchSource.R")
+source("R/process-source-data/ProcessConfigurationFiles.R")
+source("R/process-source-data/ProcessCalibrationCurveSourceFile.R")
