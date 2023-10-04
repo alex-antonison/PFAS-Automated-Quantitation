9e5dd54b882f7204e104539370bc33013d6f0879
Alexander Antonison <adantonison@gmail.com>
2023-10-04 17:58:41 -0500

    Fix issue where na were preventing values from being calculated

--- R/build-data-products/3_2_RemoveFieldBlanksFromBlankFilteredAnalyte.R
+++ R/build-data-products/3_2_RemoveFieldBlanksFromBlankFilteredAnalyte.R
@@ -1,11 +1,11 @@
 library(magrittr)
 
 extraction_batch_source <- arrow::read_parquet(
-  "../../data/processed/reference/extraction_batch_source.parquet"
+  "data/processed/reference/extraction_batch_source.parquet"
 )
 
 blank_filtered_analyte_concentration <- arrow::read_parquet(
-  "../../data/processed/build-data-products/blank_filtered_analyte_concentration_no_recovery.parquet"
+  "data/processed/build-data-products/blank_filtered_analyte_concentration_no_recovery.parquet"
 )
 
 field_blank_averaged_analyte_concentration <- extraction_batch_source %>%
@@ -14,16 +14,26 @@ field_blank_averaged_analyte_concentration <- extraction_batch_source %>%
     blank_filtered_analyte_concentration,
     by = c("batch_number", "cartridge_number")
   ) %>%
-  dplyr::filter(!is.na(individual_native_analyte_name)) %>%
   dplyr::select(
-    individual_native_analyte_name, blank_filtered_analyte_concentration_ng
+    batch_number, cartridge_number, individual_native_analyte_name, blank_filtered_analyte_concentration_ng
   ) %>%
+  dplyr::filter(!is.na(individual_native_analyte_name)) %>% 
+  dplyr::mutate(
+    blank_filtered_analyte_concentration_ng = ifelse(
+      is.na(blank_filtered_analyte_concentration_ng),
+      0.0,
+      blank_filtered_analyte_concentration_ng
+    )
+  ) %>% 
   dplyr::group_by(
     individual_native_analyte_name
   ) %>%
   dplyr::summarise(
-    field_blank_average_analyte_concentration_ng = mean(blank_filtered_analyte_concentration_ng)
-  )
+    field_blank_average_analyte_concentration_ng = mean(blank_filtered_analyte_concentration_ng),
+    field_blank_stdev_analyte_concentration_ng = sd(blank_filtered_analyte_concentration_ng),
+    field_blank_percent_rsd_analyte_concentration_ng = (field_blank_stdev_analyte_concentration_ng / field_blank_average_analyte_concentration_ng) * 100
+  ) %>%
+  readr::write_excel_csv("data/processed/build-data-products/field_blank_analyte_concentration_average_ng.csv")
 
 
 blank_filtered_analyte_concentration %>%
