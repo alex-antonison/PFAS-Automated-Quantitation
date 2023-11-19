474c76c57aaefd42b06a79a135864af219c8c6d5
Alexander Antonison <adantonison@gmail.com>
2023-11-19 01:47:38 -0600

    Create final output file

--- R/build-data-products/6_Create_Analysis_Summary_File.R
+++ R/build-data-products/6_Create_Analysis_Summary_File.R
@@ -86,45 +86,52 @@ xlsx::addDataFrame(field_blank_filtered_df, sheet = field_blank_filtered_sheet, 
 
 ########## Sheet 8 - Analyte Concentration PPT #############
 
-cal_curve_prep <- calibration_curve_output_df %>% 
+cal_curve_prep <- calibration_curve_output_df %>%
   dplyr::distinct(
     batch_number,
     individual_native_analyte_name,
     calibration_point
-  ) 
+  )
 
 analyte_concentration_ppt <- readr::read_csv("data/processed/build-data-products/analyte_concentration_ppt.csv") %>%
   dplyr::left_join(
     cal_curve_prep,
-    by = c("batch_number","individual_native_analyte_name")
-  ) %>% 
+    by = c("batch_number", "individual_native_analyte_name")
+  ) %>%
   data.frame()
 
-analyte_concentration_ppt_sheet <- xlsx::createSheet(wb, "Analyte Concentration ppt")
-xlsx::addDataFrame(analyte_concentration_ppt, sheet = analyte_concentration_ppt_sheet, row.names = FALSE)
+analyte_concentration_ppt %>%
+  dplyr::group_by(
+    batch_number,
+    cartridge_number,
+    individual_native_analyte_name
+  ) %>%
+  dplyr::summarize(count = dplyr::n()) %>%
+  dplyr::filter(count > 1)
 
-########## Sheet 9 - Analyte Concentration PPT Transposed #############
-
-# cal_curve_prep <- calibration_curve_output_df %>% 
-#   dplyr::distinct(
-#     batch_number,
-#     individual_native_analyte_name,
-#     calibration_point
-#   ) 
-# 
-# extraction_batch
-# 
-# analyte_concentration_ppt <- readr::read_csv("data/processed/build-data-products/analyte_concentration_ppt.csv") %>%
-#   dplyr::left_join(
-#     cal_curve_prep,
-#     by = c("batch_number","individual_native_analyte_name")
-#   ) %>% 
-#   data.frame()
 
 analyte_concentration_ppt_sheet <- xlsx::createSheet(wb, "Analyte Concentration ppt")
 xlsx::addDataFrame(analyte_concentration_ppt, sheet = analyte_concentration_ppt_sheet, row.names = FALSE)
-
 
 ########## Save out final file ############
 cur_time <- format(Sys.time(), "%Y-%m-%d-%I-%M")
 xlsx::saveWorkbook(wb, paste0("/Users/aantonison/OneDrive/client/UniversityOfFlorida/", cur_time, "_summary_analysis_file.xlsx"))
+
+
+########## Separate Analyte Concentration PPT Transposed #############
+
+analyte_concentration_ppt_transposed <- analyte_concentration_ppt %>%
+  dplyr::select(
+    batch_number,
+    cartridge_number,
+    sample_id,
+    county,
+    coordinates,
+    calibration_point,
+    individual_native_analyte_name,
+    analyte_concentration_ppt
+  ) %>%
+  tidyr::pivot_wider(names_from = individual_native_analyte_name, values_from = analyte_concentration_ppt, names_sep = "") %>%
+  readr::write_excel_csv(paste0("/Users/aantonison/OneDrive/client/UniversityOfFlorida/", cur_time, "_analyte_concentration_ppt_wide.csv"),
+    na = ""
+  )
