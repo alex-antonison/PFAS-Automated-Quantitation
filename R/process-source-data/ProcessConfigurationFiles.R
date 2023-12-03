0225a382585e771d6f622167e32c9d191ad8110c
Alexander Antonison <adantonison@gmail.com>
2023-12-02 21:23:02 -0600

    Add processing for removal files

--- R/process-source-data/ProcessConfigurationFiles.R
+++ R/process-source-data/ProcessConfigurationFiles.R
@@ -21,3 +21,23 @@ readxl::read_excel(
   arrow::write_parquet(
     sink = "data/processed/reference/internal_standard_max_calibration_level.parquet"
   )
+
+readxl::read_excel(
+  "data/source/reference/remove_analytes_from_batch.xlsx"
+) %>% 
+  readr::write_excel_csv(
+    "data/processed/reference/remove_analytes_from_batch.csv"
+  ) %>%
+  arrow::write_parquet(
+    "data/processed/reference/remove_analytes_from_batch.parquet"
+  )
+
+readxl::read_excel(
+  "data/source/reference/remove_analytes_from_study.xlsx"
+) %>% 
+  readr::write_excel_csv(
+    "data/processed/reference/remove_analytes_from_study.csv"
+  ) %>%
+  arrow::write_parquet(
+    "data/processed/reference/remove_analytes_from_study.parquet"
+  )
